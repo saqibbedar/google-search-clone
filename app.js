1f6e0c7ddf4b72e3ea02e11b16bba37d1761e2d5
saqibbedar <Chandio.Saqib.M@gmail.com>
2024-05-20 00:26:33 +0500

    update

--- app.js
+++ app.js
@@ -11,13 +11,17 @@ let query = document.querySelector(".query");
 let searchBtn = document.querySelector(".search-btn");
 
 searchBtn.addEventListener("click", ()=>{
-    let url = `https://www.google.com/search?q=${query.value}`;
-    window.open(url, '_self');
+    if(query.value.trim() !== ''){
+        let url = `https://www.google.com/search?q=${query.value}`;
+        window.open(url, '_self');
+    }
 })
 
 query.addEventListener("keyup", (e)=>{
-    if(e.key === 'Enter'){
-        let url = `https://www.google.com/search?q=${query.value}`;
-        window.open(url, '_self')
+if(query.value.trim() !== ''){
+        if(e.key === 'Enter'){
+            let url = `https://www.google.com/search?q=${query.value}`;
+            window.open(url, '_self')
+        }
     }
 })

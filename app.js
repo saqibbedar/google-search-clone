943da8805b74d81720532ed938336b894a12895c
saqibbedar <Chandio.Saqib.M@gmail.com>
2024-05-19 23:19:55 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+document.querySelector(".expand").addEventListener("click", (e)=>{
+    e.stopPropagation();
+    document.querySelector(".google-apps").classList.toggle("active");
+})
+
+document.body.addEventListener("click", ()=>{
+    document.querySelector(".google-apps").classList.remove("active");
+})
+
+let query = document.querySelector(".query");
+let searchBtn = document.querySelector(".search-btn");
+
+searchBtn.addEventListener("click", ()=>{
+    let url = `https://www.google.com/search?q=${query.value}`;
+    window.open(url, '_self');
+})
